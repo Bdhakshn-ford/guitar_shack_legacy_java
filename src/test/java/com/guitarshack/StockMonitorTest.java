ce325ec8b4fc1e60bea92ee936cb64638d990f6e
Bala Dakshna <bdhakshn@ford.com>
2021-04-09 11:37:04 +0100

    Add Unit Test For StockMonitor.java

---
+++ src/test/java/com/guitarshack/StockMonitorTest.java
@@ -0,0 +1,29 @@
+package com.guitarshack;
+
+import org.junit.Test;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+public class StockMonitorTest {
+
+    @Test
+    public void alertSent() {
+        Alert alert = mock(Alert.class);
+        Request request = mock(Request.class);
+        when(request.executeRequest(any(), any())).thenReturn("{\"id\": 811,\"stock\": 29,\"leadTime\": 14}", "{\"total\":60}");
+        StockMonitor stockMonitor = new StockMonitor(alert, request);
+        stockMonitor.productSold(811, 1);
+        verify(alert).send(any());
+    }
+
+    @Test
+    public void alertNotSent(){
+        Alert alert = mock(Alert.class);
+        Request request = mock(Request.class);
+        when(request.executeRequest(any(), any())).thenReturn("{\"id\": 811,\"stock\": 50,\"leadTime\": 14}", "{\"total\":60}");
+        StockMonitor stockMonitor = new StockMonitor(alert, request);
+        stockMonitor.productSold(811, 1);
+        verify(alert, never()).send(any());
+    }
+}
